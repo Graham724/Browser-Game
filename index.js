d4e63b7b40c47cbad8c5c8680f5bd46a2812434f
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-04-21 20:19:16 -0500

    css updates

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Deck
 let cards = [];
+let playerCard = [];
+let dealerCard = [];
+let cardCount = 0;
 let suits = ["hearts", "clubs", "spades", "diams"];
 let numb = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-let output = document.getElementById("output")
+let output = document.getElementById("output");
+let dealerHand = document.getElementById("dealerHand");
+let playerHand = document.getElementById("playerHand");
 
 for (s in suits) {
     let suit = suits[s][0].toUpperCase();
@@ -20,11 +25,49 @@ for (s in suits) {
     }
 }
 
-function randomCard () {
-let randomNum = Math.floor((Math.random()*52) );
-output.innerHTML += "<span style='color:" + cards[randomNum].bgcolor + "'>&" + cards[randomNum].icon + ";" + cards[randomNum].cardnum + "</span>  ";
-}
-randomCard();
-// Player 1
 
-// Dealer
+function start() {
+    shuffleDeck(cards);
+    dealNewHand();
+}
+
+function dealNewHand() {
+    playerCard = [];
+    dealerCard = [];
+    dealerHand.innerHTML = "";
+    playerHand.innerHTML = "";
+
+    for (x=0; x<2; x++){
+        dealerCard.push(cards[cardCount]);
+        dealerHand.innerHTML += cardOutput(cardCount,x);
+        cardCount++
+        playerCard.push(cards[cardCount]);
+        playerHand.innerHTML += cardOutput(cardCount,x);
+        cardCount++
+    }
+    console.log(dealerCard);
+    console.log(playerCard);
+}
+
+function cardOutput(n,x) {
+    let hpos = (x>0) ? x*60+100 : 100;
+    return '<div class="gcard ' + cards[n].icon + '" style="left:' + hpos + 'px;">  <div class="top-card suit">'
+     + cards[n].cardnum + '<br></div>  <div class="content suit"></div>  <div class="bottom-card suit">'
+      + cards[n].cardnum + '<br></div> </div>';
+}
+
+
+function shuffleDeck(array) {
+    for (let i = array.length -1; i>0; i--){
+        let j = Math.floor(Math.random() * (i+1));
+        let temp = array[i];
+        array[i] =array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+function outputCard() {
+    output.innerHTML += "<span style='color:" + cards[cardCount].bgcolor +
+     "'>" + cards[cardCount].cardnum + "&" + cards[cardCount].icon + ";</span>  ";
+}
